9ff2801f0b6d7c20965169ac8e3a0e5a6c5ab6c9
SasirekaU <98729203+SasirekaU@users.noreply.github.com>
2022-03-21 12:27:51 +0530

    Update FlightRepository.java

--- src/main/java/com/capg/springboot/repository/FlightRepository.java
+++ src/main/java/com/capg/springboot/repository/FlightRepository.java
@@ -1,5 +1,20 @@
 package com.capg.springboot.repository;
 
-public class FlightRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
-}
+import com.capg.springboot.entity.Flight;
+
+
+public abstract class FlightRepository implements JpaRepository<Flight, String>{
+
+	
+	
+	abstract Flight addFlight(Flight add);
+
+	
+	abstract Flight modifyFlight(Flight add);
+	
+
+	abstract Flight removeFlight(Flight add);
+	
+	}
